3fd86a7828defa556392cdc65e0403e2d84860af
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-10-17 22:48:07 +0600

    Create deck class

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
 
 
 class Card:
+
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
@@ -13,3 +14,19 @@ class Card:
 
     def __str__(self):
         return self.rank + ' of ' + self.suit
+
+
+class Deck:
+
+    def __init__(self):
+        self.card_list = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.card_list.append(Card(suit, rank))
+
+
+deck = Deck()
+
+for card in deck.card_list:
+    print(card)
